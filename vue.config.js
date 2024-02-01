e169d4028d9170c7c953d7999fd70bb59b434698
IlariaGalliano <ilaria.galliano7@gmail.com>
2024-02-01 16:22:15 +0100

    fixes prod V.3

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  productionSourceMap: false,
-  configureWebpack: {
-    performance: {
-      hints: process.env.NODE_ENV === "production" ? "warning" : false,
-    },
-  },
-};
